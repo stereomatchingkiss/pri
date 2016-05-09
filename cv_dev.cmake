909fd1488dd0f01a4fe330f95b42fbe8f0843b4e
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-05-10 01:15:49 +0800

    1 : forgot to specify value of LIB_SUFFIX 2 : wrong compiler version

--- cv_dev.cmake
+++ cv_dev.cmake
@@ -3,14 +3,14 @@ CMAKE_MINIMUM_REQUIRED( VERSION 3.1 )
 set(CV_LIB_VER 310)
 
 if(WIN32)
-    set(LIB_SUFFIX)
+    set(LIB_SUFFIX .lib)
     #C:\Users\yyyy\Qt\3rdLibs\opencv\opencv-3.0.0\build_vc14\bin\Release
     set(CV_PATH ${CMAKE_CURRENT_LIST_DIR}/../3rdLibs/opencv/dev)
     if(CMAKE_SIZEOF_VOID_P EQUAL 8)
         if(CMAKE_BUILD_TYPE MATCHES Release)
-            if(MSVC14) #opencv-3.1.0\bin\vc2015_x86_amd64
+            if(MSVC12) #opencv-3.1.0\bin\vc2015_x86_amd64
                 set(CV_LIB_PATH ${CV_PATH}/vc2013_64/install/x64/vc12/lib)                                  
-            endif(MSVC14)        
+            endif(MSVC12)        
         endif(CMAKE_BUILD_TYPE MATCHES Release)    
     endif(CMAKE_SIZEOF_VOID_P EQUAL 8)
 else(WIN32)
